bee6080a2963abdc4295aa4b1d623ea7cda4f2cf
DiDex <wc34fun@mail.ru>
2018-07-24 12:47:03 +0800

    add rethink connection

--- server/index.js
+++ server/index.js
@@ -1,29 +1,26 @@
-const r = require('rethinkdb');
-const io = require('socket.io')();
-
+const r = require("rethinkdb");
+const io = require("socket.io")();
 
 r.connect({
-  host: 'localhost',
+  host: "localhost",
   port: 28015,
-  db: 'awesome_whiteboard'
-}).then((connection) => {
-  io.on('connection', (client) => {
-    client.on('subscribeToTimer', (interval) => {
-      return r.table('timers')
-      .changes()
-      .run(connection)
-      .then((cursor) => {
-        cursor.each((err, timerRow) => {
-          client.emit('timer', timerRow.new_val.timestamp);
-        });
-      });
-    });
-
+  db: "awesome_whiteboard"
+}).then(connection => {
+  io.on("connection", client => {
+    client.on("subscribeToTimer", () =>
+      r
+        .table("timers")
+        .changes()
+        .run(connection)
+        .then(cursor => {
+          cursor.each((_, timerRow) => {
+            client.emit("timer", timerRow.new_val.timestamp);
+          });
+        })
+    );
   });
 });
 
-
 const port = 8000;
 io.listen(port);
-console.log('listening on port ', port);
-
+console.log("listening on port ", port);
