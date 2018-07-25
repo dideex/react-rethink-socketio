6e55e3eaac946a96b5c0bb3b1c0a00a33c324b15
DiDex <wc34fun@mail.ru>
2018-07-25 11:59:05 +0800

    server drawings subscribe

--- server/index.js
+++ server/index.js
@@ -1,23 +1,39 @@
 const r = require("rethinkdb");
 const io = require("socket.io")();
 
+function createDrawing({ connection, name }) {
+  r.table("drawings")
+    .insert({
+      name,
+      timestamp: new Date()
+    })
+    .run(connection)
+    .then(() => conole.log("created a drawing witn name", name));
+}
+
+function subscribeToDrawings({ client, connection }) {
+  r.table("drawings")
+    .changes({ include_inital: true })
+    .run(connection)
+    .then(cursor => {
+      cursor.each((_, drawingsRow) =>
+        client.emit("drawing", drawingsRow.new_van)
+      );
+    });
+}
+
 r.connect({
   host: "localhost",
   port: 28015,
   db: "awesome_whiteboard"
 }).then(connection => {
   io.on("connection", client => {
-    client.on("subscribeToTimer", () =>
-      r
-        .table("timers")
-        .changes()
-        .run(connection)
-        .then(cursor => {
-          cursor.each((_, timerRow) => {
-            client.emit("timer", timerRow.new_val.timestamp);
-          });
-        })
-    );
+    client.on("createDrawing", ({ name }) => {
+      createDrawing({ connection, name });
+    });
+    clinet.on('subscribeToDrawings', () => subscribeToDrawings({
+      client, connection
+    }))
   });
 });
 
